a718d609a5e77a0b4625bffdbcda481a2b4d1c2a
Ahmed Yasser <70852038+AhmedYasser-gu@users.noreply.github.com>
2022-04-16 14:23:03 +0200

    Update GoogleMapsUtils.ts

--- src/utils/GoogleMapsUtils.ts
+++ src/utils/GoogleMapsUtils.ts
@@ -1,5 +1,5 @@
 export const loadMapApi = () => { //Hints from https://storksnestblog.wordpress.com/2020/08/16/setting-up-google-maps-with-react-typescript/
-    const mapsURL = `https://maps.googleapis.com/maps/api/js?key=AIzaSyBQl5Nr7R5cNrPDLDf_ufwXup0V0znF7lM&libraries=geometry,places&language=no&region=NO&v=quarterly`;
+    const mapsURL = `https://maps.googleapis.com/maps/api/js?key=&libraries=geometry,places&language=no&region=NO&v=quarterly`;
     const scripts = document.getElementsByTagName('script');
     // Go through existing script tags, and return google maps api tag when found.
     for (let i = 0; i < scripts.length; i++) {
